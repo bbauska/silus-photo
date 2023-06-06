a83fdf55d036898b6b850ec201d2520e19bb8c03
Brian Bauska <brianbauska@gmail.com>
2023-06-06 10:05:37 -0700

    Update nanogallery2.js

--- javascript/nanogallery2.js
+++ javascript/nanogallery2.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function () {
   jQuery("#my_nanogallery2").nanogallery2({
   	items:[
-      { src: 'berlin1.jpg', srct: 'berlin1t.jpg', title: 'Berlin 1' },
-      { src: 'berlin2.jpg', srct: 'berlin2t.jpg', title: 'Berlin 2' },
-      { src: 'berlin3.jpg', srct: 'berlin3t.jpg', title: 'Berlin 3' }
+      { src: './photos/kitterson1.jpg', srct: 'berlin1t.jpg', title: 'Kitterson 1' },
+      { src: './photos/kitterson2.jpg', srct: 'berlin2t.jpg', title: 'Kitterson 2' },
+      { src: './photos/kitterson3.jpg', srct: 'berlin3t.jpg', title: 'Kitterson 3' }
 		],
     thumbnailWidth:  'auto',
   	thumbnailHeight: 100,
-    itemsBaseURL:    'https://nanogallery2.nanostudio.org/samples/',
+    itemsBaseURL:    'https://bbauska.github.io/silus-photo/',
     
 		locationHash:    false
   });
